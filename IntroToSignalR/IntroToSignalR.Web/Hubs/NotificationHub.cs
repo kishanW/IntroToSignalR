61ea570b04f4188cf943d87740a2650b00df3e20
Kishan Wanigasinghe <radheekakishan@gmail.com>
2020-02-27 15:02:20 -0500

    notification hub added

---
+++ IntroToSignalR/IntroToSignalR.Web/Hubs/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace IntroToSignalR.Web.Hubs
+{
+	//use this to strongly type the client-side methods for when server calls them
+	public interface INotificationHub
+	{
+	}
+
+
+	public class NotificationHub
+		: Hub //uncomment this if you do not care about strongly typing client-side methods
+		//Hub<INotificationHub> //enable this to make sure client side methods are strongly typed when called from server
+    {
+
+    }
+}
